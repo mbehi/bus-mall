0e83b90d38563cf2486f5918ce8c9ec6dc7317df
mbehi <mohsin.behi@gmail.com>
2021-02-25 21:05:28 -0600

    corrected linter errors

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ function getRandomIndex(){
   return Math.floor(Math.random() * allProducts.length);
 }
 
-// ryan showing us how to use a function 
+// ryan showing us how to use a function
 // function imgPropertyAssignment(imgIndex, imageElement){
 //   imageOne.src = allProducts(img)
 // }
@@ -115,7 +115,7 @@ function handleClick(event){
 renderProducts();
 if (totalClicks === clicksAllowed) {
 // // REMOVE EVENT LISTENER
-myContainer.removeEventListener('click', handleClick);
+  myContainer.removeEventListener('click', handleClick);
   renderChart();
 }
 
@@ -145,42 +145,42 @@ function renderChart() {
   }
   console.log(renderChart);
 
- let ctx = document.getElementById('myChart').getContext('2d');
- let myChart = new Chart(ctx, {
-  type: 'bar',
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-           label: '# of Votes',
-           data: [12, 19, 3, 5, 2, 3],
-           backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-               'rgba(255, 99, 132, 1)',
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 206, 86, 1)',
-               'rgba(75, 192, 192, 1)',
-               'rgba(153, 102, 255, 1)',
-               'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
     }
-});
+  });
 }
 myContainer.addEventListener('click', handleClick);
